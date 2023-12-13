af41628acdfb24b4bd6ec1e895c350b1a6b91d57
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-12-13 22:56:03 +0200

    Add page for registration

--- SHOP/main.go
+++ SHOP/main.go
@@ -16,11 +16,20 @@ func MainPage(w http.ResponseWriter, r *http.Request) {
 	}
 	t.ExecuteTemplate(w, "Mainpage", nil)
 }
+func Registpage(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("View/Registrpage.html", "View/header2.html", "View/footer2.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "Registrpage", nil)
+}
 func HandlePage() {
 	http.Handle("/CSS/", http.StripPrefix("/CSS/", http.FileServer(http.Dir("./CSS/"))))
 	http.Handle("/Img/", http.StripPrefix("/Img/", http.FileServer(http.Dir("./Img/"))))
+	http.Handle("/Func/", http.StripPrefix("/Func/", http.FileServer(http.Dir("./Func/"))))
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/", MainPage)
+	rtr.HandleFunc("/Registration", Registpage)
 	// rtr.HandleFunc("/create", create).Methods("GET")
 	// rtr.HandleFunc("/SaveArticle", SaveArticle).Methods("POST")
 	// rtr.HandleFunc("/post/{Id:[0-9]+}", AboutPost).Methods("GET")
