8b92f4696dc155ecedb2959ddd46cf9653636c73
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-12-14 20:46:31 +0200

    Add database conncet

--- SHOP/main.go
+++ SHOP/main.go
@@ -10,6 +10,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Users struct {
+	Id       uint
+	Email    string
+	Password string
+	TypeUser string
+}
+
+var posts = []Users{}
+
 func MainPage(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("View/Mainpage.html", "View/header.html", "View/footer.html")
 	if err != nil {
@@ -56,6 +65,44 @@ func SaveUser(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/Loginpage", http.StatusSeeOther)
 	}
 }
+func CheckUser(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/Shop.go")
+	if err != nil {
+		panic(err.Error)
+	}
+
+	defer db.Close()
+
+	send, err := db.Query("SELECT *FROM `Users`")
+	if err != nil {
+		panic(err.Error)
+	}
+	posts = []Users{}
+
+	for send.Next() {
+		var post Users
+		err = send.Scan(&post.Id, &post.Email, &post.Password, &post.TypeUser)
+		if err != nil {
+			panic(err.Error)
+		}
+
+		posts = append(posts, post)
+		if email == post.Email && password == post.Password {
+			http.Redirect(w, r, "/MainpageWithRegi", http.StatusSeeOther)
+		} else {
+			http.Redirect(w, r, "/Loginpage", http.StatusSeeOther)
+		}
+	}
+}
+func MainpageWithRegi(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("View/MainpageWithRegi.html", "View/header3.html", "View/footer3.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "MainpageWithRegi", nil)
+}
 func HandlePage() {
 	http.Handle("/CSS/", http.StripPrefix("/CSS/", http.FileServer(http.Dir("./CSS/"))))
 	http.Handle("/Img/", http.StripPrefix("/Img/", http.FileServer(http.Dir("./Img/"))))
@@ -65,6 +112,8 @@ func HandlePage() {
 	rtr.HandleFunc("/Registration", Registpage)
 	rtr.HandleFunc("/Loginpage", Loginpage)
 	rtr.HandleFunc("/SaveUser", SaveUser).Methods("POST")
+	rtr.HandleFunc("/CheckUser", CheckUser).Methods("POST")
+	rtr.HandleFunc("/MainpageWithRegi", MainpageWithRegi)
 	// rtr.HandleFunc("/create", create).Methods("GET")
 	// rtr.HandleFunc("/SaveArticle", SaveArticle).Methods("POST")
 	// rtr.HandleFunc("/post/{Id:[0-9]+}", AboutPost).Methods("GET")
