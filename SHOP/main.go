1367fbd2755e226b2049b341654a49ae045f6df3
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-12-14 14:03:32 +0200

    Add edit and login page

--- SHOP/main.go
+++ SHOP/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"text/template"
@@ -23,6 +24,38 @@ func Registpage(w http.ResponseWriter, r *http.Request) {
 	}
 	t.ExecuteTemplate(w, "Registrpage", nil)
 }
+func Loginpage(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("View/Loginpage.html", "View/header2.html", "View/footer2.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	t.ExecuteTemplate(w, "Loginpage", nil)
+}
+func SaveUser(w http.ResponseWriter, r *http.Request) {
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	userType := r.FormValue("userType")
+
+	if email == "" || password == "" || (userType != "user" && userType != "solder") {
+		fmt.Fprintf(w, "Try again")
+	} else {
+		db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/Shop.go")
+		if err != nil {
+			panic(err.Error)
+		}
+
+		defer db.Close()
+
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `Users`(`Email`,`Password`,`TypeUser`) VALUES('%s','%s','%s')", email, password, userType))
+		if err != nil {
+			panic(err.Error)
+		}
+
+		defer insert.Close()
+
+		http.Redirect(w, r, "/Loginpage", http.StatusSeeOther)
+	}
+}
 func HandlePage() {
 	http.Handle("/CSS/", http.StripPrefix("/CSS/", http.FileServer(http.Dir("./CSS/"))))
 	http.Handle("/Img/", http.StripPrefix("/Img/", http.FileServer(http.Dir("./Img/"))))
@@ -30,6 +63,8 @@ func HandlePage() {
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/", MainPage)
 	rtr.HandleFunc("/Registration", Registpage)
+	rtr.HandleFunc("/Loginpage", Loginpage)
+	rtr.HandleFunc("/SaveUser", SaveUser).Methods("POST")
 	// rtr.HandleFunc("/create", create).Methods("GET")
 	// rtr.HandleFunc("/SaveArticle", SaveArticle).Methods("POST")
 	// rtr.HandleFunc("/post/{Id:[0-9]+}", AboutPost).Methods("GET")
