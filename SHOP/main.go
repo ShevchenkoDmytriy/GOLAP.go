e3436ddcb93d5ae086d74978525b6e0ea2e3406e
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-12-15 22:27:48 +0200

    Connect database with mainpage

--- SHOP/main.go
+++ SHOP/main.go
@@ -11,21 +11,53 @@ import (
 )
 
 type Users struct {
-	Id       uint
-	Email    string
-	Password string
-	TypeUser string
+	id_user  uint
+	email    string
+	password string
+	typeUser string
+}
+type Products struct {
+	ProductId   uint
+	ProductName string
+	Price       float64
+	Description string
 }
 
 var posts = []Users{}
+var redirectURL string
+var products = []Products{}
 
 func MainPage(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("View/Mainpage.html", "View/header.html", "View/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
 	}
-	t.ExecuteTemplate(w, "Mainpage", nil)
+
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/Shop.go")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	send, err := db.Query("SELECT * FROM `Products`")
+	if err != nil {
+		panic(err.Error())
+	}
+	products = []Products{}
+
+	for send.Next() {
+		var prod Products
+		err = send.Scan(&prod.ProductId, &prod.ProductName, &prod.Price, &prod.Description)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		products = append(products, prod)
+	}
+	t.ExecuteTemplate(w, "Mainpage", products)
 }
+
 func Registpage(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("View/Registrpage.html", "View/header2.html", "View/footer2.html")
 	if err != nil {
@@ -55,7 +87,7 @@ func SaveUser(w http.ResponseWriter, r *http.Request) {
 
 		defer db.Close()
 
-		insert, err := db.Query(fmt.Sprintf("INSERT INTO `Users`(`Email`,`Password`,`TypeUser`) VALUES('%s','%s','%s')", email, password, userType))
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `Users`(`email`,`password`,`type_user`) VALUES('%s','%s','%s')", email, password, userType))
 		if err != nil {
 			panic(err.Error)
 		}
@@ -83,14 +115,15 @@ func CheckUser(w http.ResponseWriter, r *http.Request) {
 
 	for send.Next() {
 		var post Users
-		err = send.Scan(&post.Id, &post.Email, &post.Password, &post.TypeUser)
+		err = send.Scan(&post.id_user, &post.email, &post.password, &post.typeUser)
 		if err != nil {
 			panic(err.Error)
 		}
 
 		posts = append(posts, post)
-		if email == post.Email && password == post.Password {
-			http.Redirect(w, r, "/MainpageWithRegi", http.StatusSeeOther)
+		if email == post.email && password == post.password {
+			redirectURL = fmt.Sprintf("/MainpageWithRegi/%d", post.id_user)
+			http.Redirect(w, r, redirectURL, http.StatusSeeOther)
 		} else {
 			http.Redirect(w, r, "/Loginpage", http.StatusSeeOther)
 		}
@@ -103,17 +136,49 @@ func MainpageWithRegi(w http.ResponseWriter, r *http.Request) {
 	}
 	t.ExecuteTemplate(w, "MainpageWithRegi", nil)
 }
+
+// func AboutPost(w http.ResponseWriter, r *http.Request) {
+// 	t, err := template.ParseFiles("template/show.html", "template/header.html", "template/footer.html")
+// 	if err != nil {
+// 		fmt.Fprintf(w, err.Error())
+// 	}
+// 	vars := mux.Vars(r)
+// 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/golang")
+// 	if err != nil {
+// 		panic(err.Error)
+// 	}
+
+// 	defer db.Close()
+
+// 	send, err := db.Query(fmt.Sprintf("SELECT *FROM `articles` WHERE `Id`='%s'", vars["Id"]))
+// 	if err != nil {
+// 		panic(err.Error)
+// 	}
+// 	showPost = Article{}
+// 	for send.Next() {
+// 		var post Article
+// 		err = send.Scan(&post.Id, &post.Title, &post.Anons, &post.Fulltext)
+// 		if err != nil {
+// 			panic(err.Error)
+// 		}
+
+//			showPost = post
+//		}
+//		t.ExecuteTemplate(w, "show", showPost)
+//	}
 func HandlePage() {
 	http.Handle("/CSS/", http.StripPrefix("/CSS/", http.FileServer(http.Dir("./CSS/"))))
 	http.Handle("/Img/", http.StripPrefix("/Img/", http.FileServer(http.Dir("./Img/"))))
 	http.Handle("/Func/", http.StripPrefix("/Func/", http.FileServer(http.Dir("./Func/"))))
 	rtr := mux.NewRouter()
-	rtr.HandleFunc("/", MainPage)
+
 	rtr.HandleFunc("/Registration", Registpage)
 	rtr.HandleFunc("/Loginpage", Loginpage)
+	rtr.HandleFunc("/", MainPage).Methods("GET")
 	rtr.HandleFunc("/SaveUser", SaveUser).Methods("POST")
 	rtr.HandleFunc("/CheckUser", CheckUser).Methods("POST")
-	rtr.HandleFunc("/MainpageWithRegi", MainpageWithRegi)
+	rtr.HandleFunc("/MainpageWithRegi/{Id:[0-9]+}", MainpageWithRegi).Methods("GET")
+	// rtr.HandleFunc("/MainpageWithRegi/{Id:[0-9]+}", MainpageWithRegi).Methods("GET")
 	// rtr.HandleFunc("/create", create).Methods("GET")
 	// rtr.HandleFunc("/SaveArticle", SaveArticle).Methods("POST")
 	// rtr.HandleFunc("/post/{Id:[0-9]+}", AboutPost).Methods("GET")
